e807c490421cb75de5c950ddf4c98522e552fa67
Marc Tigno <tignomarcrussel@gmail.com>
2024-07-07 20:41:13 +0800

    aaa

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,26 @@
 import React, {useState} from 'react'
 import './Navbar.css';
+import DarkModeToggle from './DarkModeToggle';
 
 function Navbar() {
   return (
     <>
     <nav className="navbar">
-        <div className="navbar-logo">MyApp</div>
+        <div className="navbar-logo">Mtigno14</div>
+        <div className="navbar-right">
         <ul className="navbar-links">
+        
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li>
+          <li><a href="#services">Experience</a></li>
+          <li><a href="#contact">Projects</a></li>
           <li><a href="#contact">Contact</a></li>
+          
         </ul>
+       
+          <DarkModeToggle />
+        
+        </div>
     </nav>
     </>
   )
