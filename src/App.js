9a0002ec770771fb54baf9c557247eb3a5255b10
Marc Tigno <tignomarcrussel@gmail.com>
2024-07-17 16:57:45 +0800

    aaa

--- src/App.js
+++ src/App.js
@@ -6,9 +6,23 @@ import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
+      <Navbar />
       <div className="content">
-        hiii
+      <div className="image-container">
+          <img src="/profilepic.jpg" alt="Profile" />
+        </div>
+        <div className="text-content">
+          <div className="name-header">
+            Marc Russel Tigno
+          </div>
+          <div className="title">
+            Computer Engineer
+          </div>
+          <div className="little-about">
+            Hiii
+          </div>
+        </div>
+        
       </div>
     </div>
   );
