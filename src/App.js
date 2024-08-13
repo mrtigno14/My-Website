11b8a026f762b9426e5d6a5a6ea8721aab90261e
Marc Tigno <tignomarcrussel@gmail.com>
2024-08-13 15:34:05 +0800

    aaa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faFacebookSquare, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
 import Navbar from './components/Navbar';
 
 
-function App() {
-  return (
+function App() { 
+  return  (
     <div className="App">
       <Navbar />
       <div className="content">
@@ -23,7 +28,16 @@ function App() {
             Hello World
           </div>
         </div>
-        
+        <div className="singleCol social-media-icons">
+        <a href="https://github.com/mrtigno14">
+        <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a href="https://www.linkedin.com/in/marc-russel-tigno-a7b34423a">
+        <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+
+        </div>
+
       </div>
     </div>
   );
