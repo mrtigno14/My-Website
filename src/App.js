3884e520839792f4cdd659f4ad2882924806ca34
Marc Tigno <tignomarcrussel@gmail.com>
2024-08-14 15:07:34 +0800

    aaa

--- src/App.js
+++ src/App.js
@@ -45,7 +45,20 @@ function App() {
 
 
       <div className="about-container">
-
+      <div className="content2">
+        <div className="aboutme">
+        ABOUT ME
+        </div>
+        <div className="learnmore">
+          LEARN MORE ABOUT ME
+          </div>
+          <div className="aboutme-content">
+          Hey there! My name is Marc Russel Tigno, an aspiring Full-Stack Developer. I'm currently residing in the streets of Taguig City, Philippines. I'm also a Bachelor of Science in Computer Engineering graduate from Adamson University.<br /><br />
+          My journey in web development began when I was introduced to blockchain development by The BLOKC, where they conducted a bootcamp where they taught us how to deploy our own smart contracts in the Arbitrum Network. However, prior to that, I hated web development. My first plan was to get into game and application development. But I never thought that I would be here now, falling in love with web development and web design. <br /><br />
+          The knowledge that I learned during the bootcamp helped me see a new light in web development and I thought to myself that this might be my calling. Now here I am! I am an aspiring Full-Stack Developer, that loves to build things and make ideas turn into the real deal. I learned a new passion and am excited to see where it takes me.
+          So, if you're ready to take your online presence to the next level, I'm here to make it happen. Let's team up and bring your wildest web dreams to life!
+          </div>
+        </div>
       </div>
 
     </div>
