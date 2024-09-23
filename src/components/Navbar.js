e064ea1979042047957dfed9eaf0cd98194535f1
Marc Tigno <tignomarcrussel@gmail.com>
2024-09-23 18:11:13 +0800

    aaa

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,34 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import './Navbar.css';
 import DarkModeToggle from './DarkModeToggle';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-    <nav className="navbar">
+      <nav className="navbar">
         <div className="navbar-logo">Mtigno14</div>
-        <div className="navbar-right">
-        <ul className="navbar-links">
-        
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#experience">Experience</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
-          
-        </ul>
-       
+        <button className="toggle-button" onClick={toggleMenu}>
+          {isOpen ? 'Close' : 'Menu'}
+        </button>
+        <div className={`navbar-right ${isOpen ? 'active' : ''}`}>
+          <ul className={`navbar-links ${isOpen ? 'show' : ''}`}>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#experience">Experience</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
           <DarkModeToggle />
-        
         </div>
-    </nav>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
